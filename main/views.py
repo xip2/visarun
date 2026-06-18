d2fe6376d047bf3d6000373516954b867f7643b3
gajoz <mark2voip@gmail.com>
2026-06-18 18:11:11 +0300

    реализация базового кликбокса

--- main/views.py
+++ main/views.py
@@ -3,20 +3,30 @@ from django.shortcuts import render
 
 
 def countries(request):
-    with connection.cursor() as cursor:
-        cursor.execute("""
+    visa_free = request.GET.get("visa_free")
+
+    query = """
         SELECT
             oksm.name AS name,
             COALESCE(vr.visa_status, '-') AS visa_status,
             COALESCE(vr.days::text, '-') AS days
-            
         FROM oksm
         LEFT JOIN country_iso ci
             ON ci.country_code = oksm.code
         LEFT JOIN visa_rule vr
             ON vr.name = ci.name
-        ORDER BY oksm.name;
-        """)
+    """
+
+    params = []
+
+    if visa_free is not None:
+        query += " WHERE vr.visa_status = %s "
+        params.append("visa-free")
+
+    query += " ORDER BY oksm.name "
+
+    with connection.cursor() as cursor:
+        cursor.execute(query, params)
 
         columns = [col[0] for col in cursor.description]
         rules = [
@@ -24,6 +34,15 @@ def countries(request):
             for row in cursor.fetchall()
         ]
 
-    return render(request, "countries.html", {
-        "rules": rules
-    })
+    if request.headers.get("HX-Request"):
+        return render(
+            request,
+            "countries_table.html",
+            {"rules": rules},
+        )
+
+    return render(
+        request,
+        "countries.html",
+        {"rules": rules},
+    )
