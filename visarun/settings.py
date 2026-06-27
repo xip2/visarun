2ca6008727cc74b49113b50015876f80de61ce16
gajoz <mark2voip@gmail.com>
2026-06-27 15:53:13 +0300

    header

--- visarun/settings.py
+++ visarun/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -25,8 +25,7 @@ SECRET_KEY = 'django-insecure-&wt7xkxu9rav)4t*^77s(e3vywq0)irxewz8w)wd4tgq6o#4@b
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
@@ -76,13 +75,15 @@ WSGI_APPLICATION = 'visarun.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'postgres',
-        'USER': 'postgres',
-        'PASSWORD': '1passwd',
+        'NAME': 'vr_project',
+        'USER': 'jacob',
+        'PASSWORD': '1passwdwdwxwz',
         'HOST': 'localhost',
         'PORT': '5432',
     }
 }
+
+
 
 
 # Password validation
@@ -119,7 +120,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
